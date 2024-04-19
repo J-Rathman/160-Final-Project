b43fded63970da8b7b77e4b400ccebb39650a288
J-Rathman <jonathanrathman2004@gmail.com>
2024-04-19 13:55:45 -0400

    Website structure

--- main.py
+++ main.py
@@ -13,5 +13,25 @@ def get_index():
     return render_template("index.html")
 
 
+@app.route("/login", methods=["GET"])
+def get_login_template():
+    return render_template("accounts.html")
+
+
+@app.route("/register", methods=["GET"])
+def get_register_template():
+    return render_template("register.html")
+
+
+@app.route("/take_a_test", methods=["GET"])
+def get_test_taking_template():
+    return render_template("take_test.html")
+
+
+@app.route("/manage_tests", methods=["GET"])
+def get_manage_tests_template():
+    return render_template("manage_tests.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
