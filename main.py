42552fb82b037a2a370958973d1d3930898a06b0
J-Rathman <jonathanrathman2004@gmail.com>
2024-04-23 13:17:27 -0400

    Login/register functionality

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 from sqlalchemy import Column, Integer, String, Numeric, create_engine, text
+from flask_session import Session
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 #Needs password below
-#conn_str = "mysql://root:password@localhost/160finaldb"
-#engine = create_engine(conn_str, echo=True)
-#conn = engine.connect()
+conn_str = "mysql://root:PASSWORD@localhost/160finaldb"
+engine = create_engine(conn_str, echo=True)
+conn = engine.connect()
 
 
 @app.route("/")
 def get_index():
-    return render_template("index.html")
+    return render_template("index.html", loggedin=session.get("username"))
 
 
 @app.route("/login", methods=["GET"])
 def get_login_template():
-    return render_template("accounts.html")
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    session["username"] = request.form.get("email/username")
+    return redirect("/")
+
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    session["username"] = None
+    return redirect("/")
 
 
 @app.route("/register", methods=["GET"])
@@ -28,15 +44,16 @@ def register():
     if request.form.get("password") == request.form.get("password-confirm"):
         try:
             conn.execute(
-                #text(f"INSERT INTO accountsxp (email, username, password) VALUES (:email, :username, :password)"),
-                #request.form
+                text(f"INSERT INTO accountsxp (email, username, password, acc_type) VALUES (:email, :username, :password, :radio)"),
+                request.form
             )
             conn.commit()
-            return render_template("register.html", error=None, success="Account created! Click <a href='/login'>here</a> to login.")
+            session["username"] = request.form.get("username")
+            return render_template("register.html", error=None, success="Account created successfully!", loggedin=session.get("username"))
         except Exception as e:
             error = e.orig.args[1]
             print(error)
-            return render_template("register.html", error=error, success=None)
+            return render_template("register.html", error=error, succes=None)
     else:
         return render_template("register.html", error="Passwords do not match", success=None)
 
@@ -51,5 +68,5 @@ def get_manage_tests_template():
     return render_template("manage_tests.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
