0e3b011b9f47fe137579b0752c17e126a20619a8
J-Rathman <jonathanrathman2004@gmail.com>
2024-05-05 18:50:16 -0400

    Test grades and info

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 #Needs password below
-conn_str = "mysql://root:PASSWORD@localhost/160finaldb"
+conn_str = "mysql://root:PASSWORD/160finaldb"
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
 
@@ -38,9 +38,8 @@ def login():
         acct_type = "".join(acct_type)
         session["acct_type"] = acct_type
         return redirect("/")
-    except Exception as e:
-        error = e.orig.args[1]
-        print(error)
+    except Exception:
+        error = "Incorrect login information"
         return render_template("login.html", error=error)
 
 
@@ -70,10 +69,8 @@ def register():
             session["username"] = request.form.get("username")
             session["acct_type"] = request.form.get("radio")
             return render_template("register.html", error=None, success="Account created successfully!", loggedin=session.get("username"))
-        except Exception as e:
-            error = e.orig.args[1]
-            print(error)
-            return render_template("register.html", error=error, succes=None)
+        except Exception:
+            return render_template("register.html", error="Unexpected error encountered", succes=None)
     else:
         return render_template("register.html", error="Passwords do not match", success=None)
 
@@ -86,24 +83,24 @@ def get_test_taking_template():
 
 @app.route("/tests/<test>")
 def take_test(test):
-    if conn.execute(text(f"SELECT student_name FROM test_results WHERE test_name = {test}")).all():
+    if conn.execute(text(f"SELECT * FROM test_results WHERE test_name = '{test}' AND student_name = '{session.get('username')}'")).all():
         return render_template("index.html", loggedin=session.get("username"), success=f"You've already taken {test}")
     else:
-        questions = conn.execute(text(f"SELECT question FROM test_questions WHERE test_name = {test}"))
+        questions = conn.execute(text(f"SELECT question FROM test_questions WHERE test_name = '{test}'"))
         return render_template("testing.html", test=test, questions=questions, loggedin=session.get("username"), acct_type=session.get("acct_type"))
 
 
 @app.route("/tests/<test>", methods=["POST"])
 def submit_test(test):
-    num_questions = conn.execute(text(f"SELECT num_questions FROM tests WHERE test_name = {test}")).first()
+    num_questions = conn.execute(text(f"SELECT num_questions FROM tests WHERE test_name = '{test}'")).first()
     for i in range(0, num_questions[0]):
         conn.execute(text(f"INSERT INTO test_results (student_name, test_name, question_num, answer)"
-                          f"VALUES ('{session.get('username')}', {test}, {i + 1}, '{request.form.get(f'question-{i + 1}')}')"))
+                          f"VALUES ('{session.get('username')}', '{test}', {i + 1}, '{request.form.get(f'question-{i + 1}')}')"))
     conn.commit()
     return render_template("index.html", loggedin=session.get("username"), success="Results recorded")
 
 
-@app.route("/manage_tests", methods=["GET"])
+@app.route("/tests_and_grades", methods=["GET"])
 def get_manage_tests_template():
     tests = conn.execute(text("SELECT * FROM tests"))
     return render_template("manage_tests.html", loggedin=session.get("username"), acct_type=session.get("acct_type"), tests=tests, success=None)
@@ -111,27 +108,25 @@ def get_manage_tests_template():
 
 @app.route("/update_tests/<test>", methods=["GET"])
 def get_update_test_template(test):
-    creator = conn.execute(text(f"SELECT created_by FROM tests WHERE test_name = {test}")).first()
+    creator = conn.execute(text(f"SELECT created_by FROM tests WHERE test_name = '{test}'")).first()
     creator = "".join(creator)
-    questions = conn.execute(text(f"SELECT question FROM test_questions WHERE test_name = {test}"))
-    test = conn.execute(text(f"SELECT * FROM tests WHERE test_name = {test}")).first()
+    questions = conn.execute(text(f"SELECT question FROM test_questions WHERE test_name = '{test}'"))
+    test = conn.execute(text(f"SELECT * FROM tests WHERE test_name = '{test}'")).first()
     return render_template("update.html", loggedin=session.get("username"), acct_type=session.get("acct_type"), questions=questions, test=test, creator=creator)
 
 
 @app.route("/update_tests/<test>", methods=["POST"])
 def update(test):
     try:
-        num_question = conn.execute(text(f"SELECT num_questions FROM tests WHERE test_name = {test}")).first()
-        questions = conn.execute(text(f"SELECT question FROM test_questions WHERE test_name = {test}")).all()
-        conn.execute(text(f"UPDATE tests SET test_name = '{request.form.get('test-name')}' WHERE test_name = {test}"))
+        num_question = conn.execute(text(f"SELECT num_questions FROM tests WHERE test_name = '{test}'")).first()
+        questions = conn.execute(text(f"SELECT question FROM test_questions WHERE test_name = '{test}'")).all()
+        conn.execute(text(f"UPDATE tests SET test_name = '{request.form.get('test-name')}' WHERE test_name = '{test}'"))
         for i in range(0, num_question[0]):
-            conn.execute(text(f"UPDATE test_questions SET question = '{request.form.get(f'question-{i + 1}')}' WHERE question = '{''.join(questions[i])}' AND test_name = {test}"))
-        conn.execute(text(f"UPDATE test_questions SET test_name = '{request.form.get('test-name')}' WHERE test_name = {test}"))
+            conn.execute(text(f"UPDATE test_questions SET question = '{request.form.get(f'question-{i + 1}')}' WHERE question = '{''.join(questions[i])}' AND test_name = '{test}'"))
+        conn.execute(text(f"UPDATE test_questions SET test_name = '{request.form.get('test-name')}' WHERE test_name = '{test}'"))
         conn.commit()
         return render_template("index.html", loggedin=session.get("username"), success="Test updated successfully!")
-    except Exception as e:
-        error = e.orig.args[1]
-        print(error)
+    except Exception:
         return render_template("create_test.html", error="Unexpected error encountered", acct_type=session.get("acct_type"), loggedin=session.get("username"))
 
 
@@ -152,23 +147,22 @@ def create_test():
                               f"VALUES ('{request.form.get(f'question-{i + 1}')}', '{request.form.get("test_name")}')"))
         conn.commit()
         return render_template("index.html", loggedin=session.get("username"), success="Test created successfully!")
-    except Exception as e:
-        error = e.orig.args[1]
-        print(error)
+    except Exception:
+        error = "Error encountered"
         return render_template("create_test.html", error=error)
 
 
 @app.route("/delete/<test>", methods=["GET"])
 def delete_test_template(test):
-    test = conn.execute(text(f"SELECT * FROM tests WHERE test_name = {test}")).first()
+    test = conn.execute(text(f"SELECT * FROM tests WHERE test_name = '{test}'")).first()
     return render_template("delete_test.html", loggedin=session.get("username"), acct_type=session.get("acct_type"), test=test)
 
 
 @app.route("/delete/<test>", methods=["POST"])
 def delete_test(test):
     if request.form.get("radio") == "delete":
-        conn.execute(text(f"DELETE FROM tests WHERE test_name = {test}"))
-        conn.execute(text(f"DELETE FROM test_questions WHERE test_name = {test}"))
+        conn.execute(text(f"DELETE FROM tests WHERE test_name = '{test}'"))
+        conn.execute(text(f"DELETE FROM test_questions WHERE test_name = '{test}'"))
         conn.commit()
         return render_template("index.html", loggedin=session.get("username"), success="Test deleted successfully")
     else:
@@ -180,6 +174,37 @@ def delete_test(test):
 def get_grades_template():
     students = conn.execute(text("SELECT * FROM accounts WHERE acct_type = 'student'")).all()
     return render_template("students.html", loggedin=session.get("username"), acct_type=session.get("acct_type"), students=students)
+
+
+@app.route("/students/<student>", methods=["GET"])
+def get_student_info(student):
+    tests_taken = conn.execute(text(f"SELECT test_name FROM test_results WHERE student_name = '{student}'")).all()
+    return render_template("student_info.html", loggedin=session.get("username"), acct_type=session.get("acct_type"), student=student, tests_taken=tests_taken, results=None)
+
+
+@app.route("/students/<student>/<test>", methods=["GET"])
+def get_grade_info(student, test):
+    results = conn.execute(text(f"SELECT * FROM test_results WHERE student_name = '{student}' AND test_name = '{test}'")).all()
+    questions = conn.execute(text(f"SELECT question FROM test_questions WHERE test_name = '{test}'")).all()
+    return render_template("student_info.html", loggedin=session.get("username"), acct_type=session.get("acct_type"), questions=questions, student=student, tests_taken=None, results=results, test=test)
+
+
+@app.route("/students/<student>/<test>", methods=["POST"])
+def post_grade(student, test):
+    if conn.execute(text(f"SELECT * FROM grades WHERE test_name = '{test}' AND graded_by = '{session.get('username')}' AND student_name = '{student}'")):
+        conn.execute(text(f"DELETE FROM grades WHERE test_name = '{test}' AND graded_by = '{session.get('username')}' AND student_name = '{student}'"))
+    conn.execute(text(f"INSERT INTO grades (test_name, grade, student_name, graded_by, time_graded)"
+                      f"VALUES ('{test}', '{request.form.get('grade')} / {request.form.get('max-points')}', '{student}', '{session.get('username')}', CURRENT_TIMESTAMP())"))
+    conn.commit()
+    return render_template("index.html", loggedin=session.get("username"), success="Grade posted successfully!")
+
+
+@app.route("/tests/info/<test>", methods=["GET"])
+def get_test_info(test):
+    test_vals = conn.execute(text(f"SELECT * FROM tests WHERE test_name = '{test}'")).first()
+    num_of_students = conn.execute(text("SELECT COUNT(DISTINCT student_name) FROM test_results")).first()
+    grades = conn.execute(text(f"SELECT * FROM grades WHERE test_name = '{test}'")).all()
+    return render_template("test_info.html", loggedin=session.get("username"), acct_type=session.get("acct_type"), test_vals=test_vals, num_of_students=num_of_students, grades=grades)
 
 
 if __name__ == "__main__":
